ff1adef5917ebe29ce0de2d84472beb67383fdeb
Gabriel Carniel <gpcarniel@outlook.com>
2023-04-21 16:21:05 -0300

    rota para atualizar uma refeicao

--- src/routes/meals.ts
+++ src/routes/meals.ts
@@ -2,30 +2,17 @@ import { FastifyInstance } from 'fastify'
 import { z } from 'zod'
 import { prisma } from '../lib/prisma'
 
-const postMealsBodySchema = z.object({
-  name: z.string().min(3, { message: 'name is required' }),
-  description: z.string().min(3, { message: 'name is required' }),
-  date: z.coerce.date(),
-  hour: z.number(),
-  isTarget: z.boolean(),
-  userId: z.string(),
-})
-
-const opts = {
-  schema: {
-    response: {
-      201: {
-        type: 'object',
-        properties: {
-          hello: { type: 'string' },
-        },
-      },
-    },
-  },
-}
-
 export async function mealsRoutes(app: FastifyInstance) {
-  app.post('/', opts, async (req, rep) => {
+  app.post('/', async (req, rep) => {
+    const postMealsBodySchema = z.object({
+      name: z.string().min(3, { message: 'name is required' }),
+      description: z.string().min(3, { message: 'name is required' }),
+      date: z.coerce.date(),
+      hour: z.number(),
+      isTarget: z.boolean(),
+      userId: z.string(),
+    })
+
     const result = postMealsBodySchema.safeParse(req.body)
 
     if (!result.success) {
@@ -34,8 +21,6 @@ export async function mealsRoutes(app: FastifyInstance) {
 
     const { name, description, date, hour, isTarget, userId } = result.data
 
-    console.log(date)
-
     const user = await prisma.users.findFirst({
       where: {
         id: userId,
@@ -43,7 +28,7 @@ export async function mealsRoutes(app: FastifyInstance) {
     })
 
     if (!user) {
-      return rep.status(404).send({ error: 'User not found exists.' })
+      return rep.status(404).send({ error: 'User not found.' })
     }
 
     const meal = await prisma.meals.findFirst({
@@ -71,5 +56,62 @@ export async function mealsRoutes(app: FastifyInstance) {
     })
 
     rep.status(201)
+  })
+
+  app.put('/', async (req, rep) => {
+    const putMealsBodySchema = z.object({
+      name: z.string().min(3, { message: 'name is required' }).optional(),
+      description: z
+        .string()
+        .min(3, { message: 'name is required' })
+        .optional(),
+      date: z.coerce.date().optional(),
+      hour: z.number().optional(),
+      isTarget: z.boolean().optional(),
+      mealId: z.string(),
+    })
+    const result = putMealsBodySchema.safeParse(req.body)
+
+    if (!result.success) {
+      return { errors: result.error }
+    }
+
+    const { name, description, date, hour, isTarget, mealId } = result.data
+
+    const meal = await prisma.meals.findUnique({
+      where: {
+        id: mealId,
+      },
+      select: {
+        description: true,
+        date: true,
+        hour: true,
+        is_target: true,
+        name: true,
+        id: true,
+      },
+    })
+
+    if (!meal) {
+      return rep.status(404).send({ error: 'Meal not found.' })
+    }
+
+    const updateMeal = {
+      ...meal,
+      name: name ? name : meal.name,
+      description: description ? description : meal.description,
+      date: date ? date : meal.date,
+      hour: hour ? hour : meal.hour,
+      is_target: isTarget ? isTarget : meal.is_target,
+    }
+
+    const response = await prisma.meals.update({
+      where: { id: meal.id },
+      data: {
+        ...updateMeal,
+      },
+    })
+
+    rep.status(200).send({ success: true, data: response })
   })
 }
